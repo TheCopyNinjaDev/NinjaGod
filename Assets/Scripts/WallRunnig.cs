091ce896790e5dc06883ac946be68db178b99aeb
Игорь Хаустов <sparkcolazet@gmail.com>
2020-12-28 02:09:28 +0300

    gravity == 0

--- Assets/Scripts/WallRunnig.cs
+++ Assets/Scripts/WallRunnig.cs
@@ -1,6 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class WallRunnig : MonoBehaviour
 {
+
+    [SerializeField] private LayerMask wall;
     
+    private CharacterController _characterController;
+    private Ray _wallChecker;
+    private ScFPSController _scFPSController;
+    
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _wallChecker = new Ray(transform.position, Vector3.right);
+        _scFPSController = GetComponent<ScFPSController>();
+    }
+
+    private void Update()
+    {
+        Physics.Raycast(_wallChecker, out var hit, 1f, wall);
+        if (hit.distance == 0 && !_characterController.isGrounded)
+        {
+            _scFPSController.gravity = 0;
+        }
+    }
 }
+

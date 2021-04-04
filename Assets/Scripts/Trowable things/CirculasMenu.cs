3795c979fb62c39dde90949339eeca478772ea24
Игорь Хаустов <sparkcolazet@gmail.com>
2021-04-05 01:23:23 +0300

    Kunai toolbar is done need color fix

--- Assets/Scripts/Trowable things/CirculasMenu.cs
+++ Assets/Scripts/Trowable things/CirculasMenu.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 public class CirculasMenu : MonoBehaviour
 {
+    public Image[] circlePieces; 
     public List<MenuButton> buttons = new List<MenuButton>();
     private Vector2 _mousePosition;
-    private Vector2 _fromVector2M = new Vector2(.5f, 1f);
-    private Vector2 _centerCircle = new Vector2(.5f, .5f);
+    private readonly Vector2 _fromVector2M = new Vector2(.5f, 1f);
+    private readonly Vector2 _centerCircle = new Vector2(.5f, .5f);
     private Vector2 _toVector2M;
     public int menuItems;
     public int curMenuItem;
@@ -16,39 +17,62 @@ public class CirculasMenu : MonoBehaviour
 
     private void Awake() 
     {
-        menuItems = buttons.Count;
-        foreach(MenuButton button in buttons)
-        {
-            button.sceneimage.color = button.NormalColor;
-        } 
         curMenuItem = 0;
         _oldMenuItem = 0;
     }
 
-    private void Update() 
+    private void Update()
     {
         GetCurrentMenuItem();
+        if(Input.GetKeyUp("l"))
+            AddItem();
         if(Input.GetKey("k"))
             ButtonAction();
     }
 
-    public void GetCurrentMenuItem()
+    private void GetCurrentMenuItem()
     {
         _mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         _toVector2M = new Vector2(_mousePosition.x/Screen.width, _mousePosition.y/Screen.height);
-        float angle = (Mathf.Atan2(_fromVector2M.y - _centerCircle.y, _fromVector2M.x - _centerCircle.x) - Mathf.Atan2(_toVector2M.y - _centerCircle.y, _toVector2M.x - _centerCircle.x)) * Mathf.Rad2Deg;
+        var angle = (Mathf.Atan2(_fromVector2M.y - _centerCircle.y, _fromVector2M.x - _centerCircle.x) - Mathf.Atan2(_toVector2M.y - _centerCircle.y, _toVector2M.x - _centerCircle.x)) * Mathf.Rad2Deg;
         if(angle < 0)
             angle += 360;
-        curMenuItem = (int)(angle / (360 / menuItems));
-        if(curMenuItem != _oldMenuItem)
+        if(menuItems > 0)
+            curMenuItem = (int)(angle / (360 / menuItems));
+        if (curMenuItem == _oldMenuItem) return;
+        buttons[_oldMenuItem].sceneimage.color = buttons[_oldMenuItem].NormalColor;
+        _oldMenuItem = curMenuItem;
+        buttons[curMenuItem].sceneimage.color = buttons[curMenuItem].HighlightedColor;
+    }
+    
+
+    public void AddItem()
+    {
+        if (menuItems >= circlePieces.Length) return;
+        circlePieces[menuItems].gameObject.SetActive(true);
+        buttons.Add(new MenuButton(menuItems));
+        menuItems = buttons.Count;
+        foreach (var piece in circlePieces)
         {
-            buttons[_oldMenuItem].sceneimage.color = buttons[_oldMenuItem].NormalColor;
-            _oldMenuItem = curMenuItem;
-            buttons[curMenuItem].sceneimage.color = buttons[curMenuItem].HighlightedColor;
+            piece.fillAmount = 1.0f / menuItems;
         }
+
+        var rotZ = 180;
+        for (int i = 0; i < circlePieces.Length; i++)
+        {
+            circlePieces[i].gameObject.GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, rotZ);
+            rotZ -= 360 / menuItems;
+            buttons[i].sceneimage = circlePieces[i];
+        }
+        
+        foreach(var button in buttons)
+        {
+            button.sceneimage.color = button.NormalColor;
+        }
+        
     }
 
-    public void ButtonAction()
+    private void ButtonAction()
     {
         buttons[curMenuItem].sceneimage.color = buttons[curMenuItem].PressedColor;
         if(curMenuItem == 0)
@@ -58,9 +82,14 @@ public class CirculasMenu : MonoBehaviour
 [System.Serializable]
     public class MenuButton
     {
+        public MenuButton(int name)
+        {
+            this.name = name.ToString();
+        }
+        
         public string name;
         public Image sceneimage;
-        public Color NormalColor;
-        public Color HighlightedColor;
-        public Color PressedColor;
+        public Color NormalColor = Color.black;
+        public Color HighlightedColor = Color.red;
+        public Color PressedColor = Color.white;
     }

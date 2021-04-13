133b8bc5776c7a220075de83509ff7a3bb6beac9
Игорь Хаустов <sparkcolazet@gmail.com>
2021-04-13 20:56:01 +0300

    meta files of Teleport kunai commit

---
+++ Assets/Scripts/Trowable things/TeleportKunai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+namespace Trowable_things
+{
+    public class TeleportKunai: Kunai
+    {
+        private Transform _player;
+        protected override void Awake()
+        {
+            base.Awake();
+            _player = GameObject.FindWithTag("Player").transform;
+        }
+
+        private void LateUpdate()
+        {
+            // Teleports player to the last thrown kunai
+            if (Input.GetKey("t") && FightSystem.CurrentKunai.gameObject.GetComponentInChildren<Kunai>().readyToTeleport)
+            {
+                var position = FightSystem.CurrentKunai.transform.position;
+                _player.position = new Vector3(position.x, 
+                    position.y + 2, position.z);
+            }
+        }
+    }
+}

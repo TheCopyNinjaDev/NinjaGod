6ca9f08011d6da454cfb972f9f9749d5e3c92ea8
Игорь Хаустов <sparkcolazet@gmail.com>
2021-04-19 16:14:45 +0300

    Kunai explosion is ready and has it's vfx

--- Assets/Scripts/Trowable things/ExplosiveKunai.cs
+++ Assets/Scripts/Trowable things/ExplosiveKunai.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 public class ExplosiveKunai : Kunai
 {
     [SerializeField] private float _explosionRadius = 10;
+    [SerializeField] private float _explosionForce = 500;
+    [SerializeField] private ParticleSystem _explosion;
     
     protected override void OnCollisionEnter(Collision collision)
     {
-        base.OnCollisionEnter(collision);
+        if (!ReadyToStick) return;
+        Rb.constraints  = RigidbodyConstraints.FreezeAll;
         var things = Physics.OverlapSphere(transform.position, _explosionRadius);
         foreach (var thing in things)
         {
             if(thing.GetComponent<Rigidbody>())
-                thing.GetComponent<Rigidbody>().AddExplosionForce(500, transform.position, _explosionRadius);
+                thing.GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
         }
+
+        Instantiate(_explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }

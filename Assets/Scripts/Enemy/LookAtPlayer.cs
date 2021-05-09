79dd9825dd9ebfdb1a1a0cecea4f8b2e10d7afef
Игорь Хаустов <sparkcolazet@gmail.com>
2021-05-09 17:07:51 +0300

    before animator changes

---
+++ Assets/Scripts/Enemy/LookAtPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class LookAtPlayer: MonoBehaviour
+    {
+        private Transform _playerPos;
+        
+        private void Start()
+        {
+            _playerPos = GameObject.FindWithTag("Player").transform;
+        }
+
+        private void Update()
+        {
+            var dir = _playerPos.position - transform.position;
+            if (Vector3.Angle(dir, transform.forward) > 45)
+            {
+                Vector3.RotateTowards(transform.forward, dir, 30, 30);
+            }
+        }
+    }
+}
